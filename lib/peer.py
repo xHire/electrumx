c7b7a8cbf09da910f2d6a1ee6bcc057ff7c2370e
Neil Booth <kyuupichan@gmail.com>
2017-02-13 08:19:49 +0900

    Full licence boilerplate on lib/ files.

--- lib/peer.py
+++ lib/peer.py
@@ -23,6 +23,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+'''Representation of a peer server.'''
+
 import re
 from ipaddress import ip_address
 
